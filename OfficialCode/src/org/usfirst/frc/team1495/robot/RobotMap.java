07c4558cd47074543471f76bdbe5f2dbd59fbbe0
Skinny229 <rlopez3@avongrove.org>
2017-01-11 15:35:39 -0500

    ..

--- OfficialCode/src/org/usfirst/frc/team1495/robot/RobotMap.java
+++ OfficialCode/src/org/usfirst/frc/team1495/robot/RobotMap.java
@@ -14,5 +14,6 @@ public class RobotMap {
     // If you are using multiple modules, make sure to define both the port
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
+    // public static int rangefinderModule = 1; This is a test testing the bin
+	
 }
