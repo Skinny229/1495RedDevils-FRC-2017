a6255146837c41ec9069373929bdefa907d8b5f5
Brendan Duff <bjdjr2000@gmail.com>
2017-01-12 15:14:57 -0500

    Ultrasonic Subsystem

--- SteamWorksRobot/src/org/usfirst/frc/team1495/robot/RobotMap.java
+++ SteamWorksRobot/src/org/usfirst/frc/team1495/robot/RobotMap.java
@@ -24,4 +24,7 @@ public class RobotMap {
 	
 	public static final int JOYSTICK_PORT = 1;
 	
+	// Ultrasonic sensor ports
+	public static final int ULTRASONIC_PING = 0;
+	public static final int ULTRASONIC_ECHO = 0;
 }
